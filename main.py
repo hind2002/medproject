ce1ceb7f87c67f0db5ab964a0e762723171547da
hind2002 <106165134+hind2002@users.noreply.github.com>
2023-03-16 03:12:21 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
-"""
-Ce module définit une fonction pour la multiplication de deux nombres.
-"""
-
-def multiply(a, b):
+def multiplication(a, b):
     """
-    Cette fonction prend deux nombres en entrée et retourne leur produit.
+    Multiplie deux nombres a et b et retourne le résultat.
+    
+    Args:
+    a (float): Le premier nombre.
+    b (float): Le deuxième nombre.
+    
+    Returns:
+    float: Le résultat de la multiplication de a et b.
     """
     return a * b
