7542d244bcc4245f71012d0d14fe6b1df5573c97
hind2002 <hanedhind951@gmail.com>
2023-03-16 02:59:20 +0100

    test3

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 def power_of_3(num):
-    return num ** 3
+    """
+    Cette fonction calcule la puissance en 3 d'un nombre donné.
 
+    Args:
+        num (int): le nombre dont on veut calculer la puissance en 3.
+
+    Returns:
+        int: la puissance en 3 du nombre donné.
+    """
+    return num ** 3
