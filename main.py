9bd6ac46dba399edc46e64ef242339accd1c38f6
hind2002 <hanedhind951@gmail.com>
2023-03-16 02:36:34 +0100

    test1

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
 def cubic(a):
+    """
+    Cette fonction calcule le cube de son argument.
+
+    Args:
+        a (float): Le nombre à mettre au cube.
+
+    Returns:
+        float: Le résultat du cube de a.
+    """
     return a*a*a
